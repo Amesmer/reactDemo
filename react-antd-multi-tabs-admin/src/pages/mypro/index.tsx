664c2c46ba9720fe382a887d9bfe78633b919072
dongyue <535026442@qq.com>
2022-08-22 15:45:56 +0800

    change antd to 4.22.7

--- react-antd-multi-tabs-admin/src/pages/mypro/index.tsx
+++ react-antd-multi-tabs-admin/src/pages/mypro/index.tsx
@@ -1,7 +1,59 @@
 import React, { useEffect, FC, useState } from 'react'
 import { withRouter } from 'react-router-dom';
 import styles from './myPro.module.less'
+import type { DataNode, TreeProps } from 'antd/es/tree';
+import { Tree } from 'antd';
+const treeData: DataNode[] = [
+    {
+      title: 'parent 1',
+      key: '0-0',
+      children: [
+        {
+          title: 'parent 1-0',
+          key: '0-0-0',
+          disabled: true,
+          children: [
+            {
+              title: 'leaf',
+              key: '0-0-0-0',
+              disableCheckbox: true,
+            },
+            {
+              title: 'leaf',
+              key: '0-0-0-1',
+            },
+          ],
+        },
+        {
+          title: 'parent 1-1',
+          key: '0-0-1',
+          children: [{ title: <span style={{ color: '#1890ff' }}>sss</span>, key: '0-0-1-0' }],
+        },
+      ],
+    },
+  ];
 
+const TReeCom:FC=()=>{
+    const onSelect: TreeProps['onSelect'] = (selectedKeys, info) => {
+        console.log('selected', selectedKeys, info);
+      };
+    
+      const onCheck: TreeProps['onCheck'] = (checkedKeys, info) => {
+        console.log('onCheck', checkedKeys, info);
+      };
+    
+      return (
+        <Tree
+          checkable
+          defaultExpandedKeys={['0-0-0', '0-0-1']}
+          defaultSelectedKeys={['0-0-0', '0-0-1']}
+          defaultCheckedKeys={['0-0-0', '0-0-1']}
+          onSelect={onSelect}
+          onCheck={onCheck}
+          treeData={treeData}
+        />
+      );
+}
 const MyPropage: FC = () => {
     const [ count, setcount ] = useState<number>(0)
     useEffect(() => {
@@ -16,7 +68,7 @@ const MyPropage: FC = () => {
             <h5>this is mypage</h5>
             <p>{count}</p>
             <button className={styles.mybtn}  onClick={btnClick}>+1</button>
-
+            <TReeCom/>
         </div>
     )
 }
